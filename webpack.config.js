f157c3393c328c242ea739676c807944d9127ae0
Lily <mrs.esmirn@gmail.com>
2025-04-22 11:33:59 +0400

    feat: add obfuscation

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = {
     entry: './src/scripts/mkb.js',
@@ -19,6 +20,14 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'css/main.css',
         }),
+        // Плагин для обфускации
+        new WebpackObfuscator({
+            rotateStringArray: true,
+            stringArray: true,
+            stringArrayEncoding: ['base64'],
+            deadCodeInjection: true,
+            deadCodeInjectionThreshold: 0.4,
+        }, ['scripts/mkb.js']),
     ],
     module: {
         rules: [
