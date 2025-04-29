41514a4ec9740d0522a503960db78ee91897c424
Lily <mrs.esmirn@gmail.com>
2025-04-29 17:53:06 +0400

    fix: tests

--- src/scripts/crypto.js
+++ src/scripts/crypto.js
@@ -3,13 +3,23 @@ import CryptoJS from 'crypto-js';
 const tmpKey = "secretKey";
 const keyUrl = "https://easymed.pro/key"; //TODO: provide the correct url
 
+// export async function decryptData(encryptedData) {
+//     //let key = await fetch(keyUrl);
+//
+//     let decrypted = CryptoJS.AES.decrypt(encryptedData, tmpKey);
+//     return decrypted.toString(CryptoJS.enc.Utf8);
+// }
+
+
 export async function decryptData(encryptedData) {
-    //let key = await fetch(keyUrl);
-
-    let decrypted = CryptoJS.AES.decrypt(encryptedData, tmpKey);
-    return decrypted.toString(CryptoJS.enc.Utf8);
+    try {
+        let decrypted = CryptoJS.AES.decrypt(encryptedData, tmpKey);
+        return decrypted.toString(CryptoJS.enc.Utf8);
+    } catch (err) {
+        console.error("Decryption failed:", err);
+        return '';
+    }
 }
-
 // TODO: remove after CryptoJS testing
 // export function encryptData(encryptedData) {
 //     //let key = await fetch(keyUrl);
