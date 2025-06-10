3c1d7305b49c86ceb952454aaa1f15d770481664
Lily <mrs.esmirn@gmail.com>
2025-06-10 14:39:11 +0400

    fix: styles for duration/plan text

--- src/scripts/mkb.js
+++ src/scripts/mkb.js
@@ -1406,14 +1406,14 @@ function createTreatBlock(parentElem, treatData, prevName) {
   if (treatData.plan) {
     const treatPlan = document.createElement('p');
     treatPlan.classList.add('block__comment', 'block__comment--plan');
-    treatPlan.innerHTML = 'Схема лечения: ' + treatData.plan;
+    treatPlan.innerHTML = '<b>Схема лечения: </b>>' + treatData.plan;
     treatContainer.appendChild(treatPlan);
   }
 
   if (treatData.duration) {
     const treatDuration = document.createElement('p');
     treatDuration.classList.add('block__comment', 'block__comment--duration');
-    treatDuration.innerHTML = 'Длительность курса: ' + treatData.duration;
+    treatDuration.innerHTML = '<b>Длительность курса: </b>' + treatData.duration;
     treatContainer.appendChild(treatDuration);
   }
 
